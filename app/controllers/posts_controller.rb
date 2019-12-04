c1adaac2d154264191ec12db694938d9f7d78ed3
kudou_kyohei <kudou.pg1010@gmail.com>
2019-12-04 21:44:07 +0900

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,10 +5,12 @@ class PostsController < ApplicationController
   end
 
   def create
+    Post.create(post_params)
+    redirect_to posts_path
   end
 
   private
   def post_params
-    
+    params.require(:post).permit(:content).merge(user_id: current_user.id)
   end
 end
